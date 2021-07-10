929d0c8ab209ef6a7f12cf2e8e5c4f866814d7f7
liyanjie-iri <198622lyj@163.com>
2021-07-10 12:05:50 +0800

    update test_loguru.py

--- test_loguru.py
+++ test_loguru.py
@@ -6,5 +6,11 @@ def test():
     logger.warning("warning level message") 
     logger.critical("critical level message") 
 
+def test_rotation():    
+    logger.add('output/file_{time}.log', rotation = "200KB")
+    for n in range(10000):
+        logger.info(f"test - {n}")
+
 if __name__ == '__main__':
     test()
+    test_rotation()
