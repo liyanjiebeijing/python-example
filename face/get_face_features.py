48f91244093fac191779372f2adaaf77e88ecffc
liyanjie-iri <liyanjie-iri@360.cn>
2021-08-09 20:16:16 +0800

    add get face features codes

--- face/get_face_features.py
+++ face/get_face_features.py
@@ -13,6 +13,7 @@ import base64
 import numpy as np
 from face_utils import *
 from tqdm import tqdm
+import threading
 
 import platform 
 print("python vesion:", platform.python_version())
@@ -53,12 +54,25 @@ def compare_image_face_dir(img_dir):
             image_content = base64.b64encode(f.read())
             result = detect_face(image_content)
 
-        if len(result['faces']) > 0:                                
+        if len(result['faces']) > 0 and result['faces'][0]['valid']:
             face = result['faces'][0]
-            if 'fea_20210326' in face: features.append((name, face['fea_20210326'], img))            
+            # if 'fea_20210326' in face: features.append((name, face['fea_20210326'], img)) 
+
+            features.append((name, np.load(os.path.join(img_dir + '_feature', name + '.fea20210326.npy')), img))
            
     visilize_similarity(features, 'face_similarity')
 
+
+
+class PostThread (threading.Thread):
+    def __init__(self, img_b64, img_name):
+        threading.Thread.__init__(self)
+        self.img_b64 = img_b64
+        self.img_name = img_name   
+        self.result = {}
+
+    def run(self):
+        self.result = detect_face(self.img_b64)
 
 
 def get_face_features_dir(img_dir):
@@ -68,27 +82,37 @@ def get_face_features_dir(img_dir):
 
     names = os.listdir(img_dir)
     names.sort()
+    names = names[:32]
+    threads = []
+    img_count = 0
     for name in tqdm(names):
+        img_count += 1
         if name[-4:] != '.jpg' and name[-4:] != '.jpeg' and name[-4:] != '.png':
             continue
 
         img_path = os.path.join(img_dir, name)        
         assert(os.path.exists(img_path))
-        img = cv2.imread(img_path)
-        if not isinstance(img, np.ndarray):
-            print('failed to load image: %s' % (name))
-            continue
         
         with open(img_path, 'rb') as f:
-            image_content = base64.b64encode(f.read())
-            result = detect_face(image_content)
+            image_b64 = base64.b64encode(f.read())                            
+            one_thread = PostThread(image_b64, name)
+            threads.append(one_thread)
 
-        #save features
-        if len(result['faces']) > 0 and result['faces'][0]['valid']:                                
-            face = result['faces'][0]
-            np.save(os.path.join(feature_dir, name + '.fea1906.npy'), face['fea_1906'])
-            np.save(os.path.join(feature_dir, name + '.fea_20210326.npy'), face['fea_20210326'])
+        if len(threads) > 50 or img_count == len(names):
+            for each in threads:
+                each.start()
 
+            for each in threads:
+                each.join()
+
+            #save features
+            for thread in threads:
+                if len(thread.result['faces']) > 0 and thread.result['faces'][0]['valid']:
+                    face = thread.result['faces'][0]
+                    np.save(os.path.join(feature_dir, thread.img_name + '.fea1906.npy'), face['fea_1906'])
+                    np.save(os.path.join(feature_dir, thread.img_name + '.fea20210326.npy'), face['fea_20210326'])
+
+            threads = []
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -97,6 +121,7 @@ if __name__ == "__main__":
 
     if os.path.isdir(sys.argv[1]):
         get_face_features_dir(sys.argv[1])
+        # compare_image_face_dir(sys.argv[1])
     else:
         print('usage: %s <img_dir>')
 
